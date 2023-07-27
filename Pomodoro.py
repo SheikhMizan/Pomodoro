22b3402e87c703536fcedaaf52a59792c37db3e0
Sheikh MiZan <sheikhmizanbd@gmail.com>
2023-07-27 20:16:14 +0600

    Update Pomodoro.py

--- Pomodoro.py
+++ Pomodoro.py
@@ -1,18 +1,13 @@
-#Pomodoro Application
-'''Requires playsound module inorder to play the music. To install --- "pip install playsound". '''
-
-#Module to play audio files
 from playsound import playsound
 import time
 
-duration = int(input('Enter Duration for your Pomodoro{in minutes}: '))
-
+duration = int(input('Enter Duration for your Pomodoro in minutes: '))
 
 print(time.asctime())
 print('Pomodoro Started. Focus....')
 for i in range(10):
     playsound('TickTick.mp3')
-    #Giving 0.8 as argument works best for sleeping for 1second
+    #Giving 0.8 as argument works best
     time.sleep(0.8)
 
 print('Pomodoro Completed!')
